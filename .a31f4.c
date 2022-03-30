60a8502f3ee4e47dfc6c4c8aa64d5a2c85097d4f
stavrosal <102695313+stavrosal@users.noreply.github.com>
2022-03-30 22:52:52 +0300

    Rename a31f4.c to .a31f4.c

---
+++ .a31f4.c
@@ -0,0 +1,340 @@
+// Filename a31f4.c
+/*               ΥΛΟΠΟΙΗΣΗ ΣΥΝΔΕΔΕΜΈΝΗΣ ΛΊΣΤΑΣ ΜΕ ΠΙΝΑΚΑ
+         TA ΣΤΟΙΧΕΙΑ ΤΗΣ ΣΥΝΔΕΔΕΜΕΝΗΣ ΛΙΣΤΑΣ ΕΙΝΑΙ ΤΥΠΟΥ int
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NumberOfNodes 25    /*όριο μεγέθους της συνδεδεμένης λίστας,*/
+#define NilValue -1          // ειδική μεδενικη τιμη δείχνει το τέλος της Συνδ.λίστας
+
+
+typedef int ListElementType; /*τύπος δεδομένων για τα στοιχεία της συνδεδεμένης λίστας,*/
+
+typedef int ListPointer;
+
+typedef struct {
+    ListElementType Data;
+    ListPointer  Next;
+} NodeType;
+
+typedef struct {
+    ListElementType klash;
+    ListElementType Freq;
+    ListPointer  Next;
+} NodeFreq;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+void InitializeStoragePool(NodeType Node[], ListPointer *FreePtr);
+void CreateList(ListPointer *List);
+boolean EmptyList(ListPointer List);
+boolean FullList(ListPointer FreePtr);
+void GetNode(ListPointer *P, ListPointer *FreePtr, NodeType Node[]);
+void ReleaseNode(NodeType Node[NumberOfNodes], ListPointer P, ListPointer *FreePtr);
+void Insert(ListPointer *List, NodeType Node[],ListPointer *FreePtr, ListPointer PredPtr, ListElementType Item);
+void Delete(ListPointer *List, NodeType Node[], ListPointer *FreePtr, ListPointer PredPtr);
+void TraverseLinked(ListPointer List, NodeType Node[]);
+
+void FreqInitializeStoragePool(NodeFreq Node[], ListPointer *FreePtr);
+void TraverseFreqLinked(ListPointer List, NodeFreq Node[]);
+void InsertFreq(ListPointer *List, NodeFreq Node[],ListPointer *FreePtr, ListPointer PredPtr, ListElementType Item1, ListElementType Item2);
+void GetNodeFreq(ListPointer *P, ListPointer *FreePtr, NodeFreq Node[]);
+
+void GetFrequencies(NodeType Node[], NodeFreq domh[], ListPointer *deikths, ListPointer *FreePtr1, ListPointer PredPtr1, int obs);
+
+int main(){
+    int n, i, item;
+    NodeType lista[NumberOfNodes];
+    ListPointer FreePtr, Ptr, PredPtr;
+
+    NodeFreq syxn[NumberOfNodes];
+    ListPointer deikths, FreePtr1, PredPtr1;
+
+    do {
+        printf("Enter observations number: ");
+        scanf("%d", &n);
+    }while (n < 0 || n > NumberOfNodes);
+
+
+    InitializeStoragePool(lista, &FreePtr);
+    CreateList(&Ptr);
+    PredPtr = NilValue;
+
+    for (i=0; i<n; i++){
+        item = 0;
+        do{
+            printf("Enter an observation in [0,9]: ");
+            scanf("%d", &item);
+        }while (item < 0 || item > 9);
+        Insert(&Ptr, lista, &FreePtr, PredPtr, item);
+    }
+    printf("\n1h Lista Parathrhsewn\n");
+    TraverseLinked(Ptr, lista);
+
+    FreqInitializeStoragePool(syxn, &FreePtr1);
+    CreateList(&deikths);
+    PredPtr1 = NilValue;
+
+    printf("\n2h Lista Syxnothtwn\n");
+    GetFrequencies(lista, syxn, &deikths, &FreePtr1, PredPtr1, n);
+    TraverseFreqLinked(deikths, syxn);
+
+    return 0;
+}
+
+void GetFrequencies(NodeType Node[], NodeFreq domh[], ListPointer *deikths, ListPointer *FreePtr1, ListPointer PredPtr1, int obs){
+    int i;
+    for (i=0; i<10; i++){
+        InsertFreq(deikths, domh, FreePtr1, PredPtr1, i, 0);
+        //domh[i].klash = i;
+    }
+
+    for (i=0; i<obs; i++){
+        domh[Node[i].Data].Freq = domh[Node[i].Data].Freq + 1 ;
+    }
+}
+
+void InitializeStoragePool(NodeType Node[], ListPointer *FreePtr)
+/* Δέχεται:   Τον πίνακα Node και τον δείκτη FreePtr που δείχνει στον
+                πρώτο διαθέσιμο κόμβο.
+  Λειτουργία: Αρχικοποιεί τον πίνακα Node ως συνδεδεμένη λίστα συνδέοντας μεταξύ
+                τους διαδοχικές εγγραφές του πίνακα,
+                και αρχικοποιεί τον δείκτη FreePtr .
+  Επιστρέφει: Τον τροποποιημένο πίνακα Node και τον
+                δείκτη FreePtr του πρώτου διαθέσιμου κόμβου
+*/
+{
+   int i;
+
+    for (i=0; i<NumberOfNodes-1;i++)
+    {
+        Node[i].Next=i+1;
+        Node[i].Data=-1;    /* δεν είναι αναγκαίο η απόδοση αρχικής τιμής στο πεδίο των δεδομένων, βοηθάει στην εκτύπωση */
+    }
+    Node[NumberOfNodes-1].Next=NilValue;
+    Node[NumberOfNodes-1].Data=-1;
+    *FreePtr=0;
+}
+
+void FreqInitializeStoragePool(NodeFreq Node[], ListPointer *FreePtr)
+/* Δέχεται:   Τον πίνακα Node και τον δείκτη FreePtr που δείχνει στον
+                πρώτο διαθέσιμο κόμβο.
+  Λειτουργία: Αρχικοποιεί τον πίνακα Node ως συνδεδεμένη λίστα συνδέοντας μεταξύ
+                τους διαδοχικές εγγραφές του πίνακα,
+                και αρχικοποιεί τον δείκτη FreePtr .
+  Επιστρέφει: Τον τροποποιημένο πίνακα Node και τον
+                δείκτη FreePtr του πρώτου διαθέσιμου κόμβου
+*/
+{
+   int i;
+
+    for (i=0; i<NumberOfNodes-1;i++)
+    {
+        Node[i].Next=i+1;
+        Node[i].klash=0;
+        Node[i].Freq=0;     /* δεν είναι αναγκαίο η απόδοση αρχικής τιμής στο πεδίο των δεδομένων, βοηθάει στην εκτύπωση */
+    }
+    Node[NumberOfNodes-1].Next=NilValue;
+    Node[NumberOfNodes-1].klash=-1;
+    Node[NumberOfNodes-1].Freq=-1;
+    *FreePtr=0;
+}
+
+void CreateList(ListPointer *List)
+/* Λειτουργία: Δημιουργεί μια κενή συνδεδεμένη λίστα.
+   Επιστρέφει: Έναν (μηδενικό) δείκτη που δείχνει σε κενή λίστα
+*/
+{
+  *List=NilValue;
+}
+
+boolean EmptyList(ListPointer List)
+/* Δέχεται:    Έναν δείκτη List που δείχνει σε μια συνδεδεμένη λίστα.
+   Λειτουργία: Ελέγχει αν η συνδεδεμένη λίστα είναι κενή.
+   Επιστρέφει: True αν η συνδεδεμένη λίστα είναι κενή και false διαφορετικά
+*/
+{
+  return (List==NilValue);
+}
+
+boolean FullList(ListPointer FreePtr)
+/* Δέχεται:    Μια συνδεδεμένη λίστα.
+   Λειτουργία: Ελέγχει αν η συνδεδεμένη λίστα είναι γεμάτη.
+   Επιστρέφει: True αν η συνδεδεμένηλίστα είναι γεμάτη, false διαφορετικά
+*/
+{
+  return (FreePtr == NilValue);
+}
+
+void GetNode(ListPointer *P, ListPointer *FreePtr, NodeType Node[])
+/* Δέχεται:   Τον πίνακα Node και τον δείκτη FreePtr.
+  Λειτουργία: Αποκτά έναν "ελεύθερο" κόμβο που τον δείχνει ο δείκτης P.
+  Επιστρέφει: Τον δείκτη P και τον τροποποιημένο  δείκτη FreePtr
+                που δεικτοδοτεί στο πρώτο διαθέσιμο κόμβο
+*/
+{
+  *P = *FreePtr;
+  if (!FullList(*FreePtr))
+    *FreePtr =Node[*FreePtr].Next;
+}
+
+void GetNodeFreq(ListPointer *P, ListPointer *FreePtr, NodeFreq Node[])
+/* Δέχεται:   Τον πίνακα Node και τον δείκτη FreePtr.
+  Λειτουργία: Αποκτά έναν "ελεύθερο" κόμβο που τον δείχνει ο δείκτης P.
+  Επιστρέφει: Τον δείκτη P και τον τροποποιημένο  δείκτη FreePtr
+                που δεικτοδοτεί στο πρώτο διαθέσιμο κόμβο
+*/
+{
+  *P = *FreePtr;
+  if (!FullList(*FreePtr))
+    *FreePtr =Node[*FreePtr].Next;
+}
+
+void ReleaseNode(NodeType Node[], ListPointer P, ListPointer *FreePtr)
+/* Δέχεται:    Τον πίνακα Node, που αναπαριστά τη δεξαμενή των διαθέσιμων κόμβων,
+                έναν δείκτη TempPtr και τον δείκτη FreePtr.
+   Λειτουργία: Επιστρέφει στη δεξαμενή τον κόμβο στον οποίο δείχνει ο TempPtr.
+   Επιστρέφει: Τον τροποποιημένο πίνακα Node και τον δείκτη FreePtr
+*/
+{
+  Node[P].Next =*FreePtr;
+  Node[P].Data = -1; /* Οχι αναγκαία εντολή, βοηθητική για να φαίνονται στην
+                        εκτύπωση οι διαγραμμένοι κόμβοι */
+  *FreePtr =P;
+}
+
+void Insert(ListPointer *List, NodeType Node[],ListPointer *FreePtr, ListPointer PredPtr, ListElementType Item)
+/* Δέχεται:    Μια συνδεδεμένη λίστα, τον πίνακα Node, τον δείκτη PredPtr και
+                ένα στοιχείο Item.
+  Λειτουργία: Εισάγει στη συνδεδεμένη λίστα, αν δεν είναι γεμάτη, το στοιχείο
+                Item μετά από τον κόμβο στον οποίο δείχνει ο δείκτης PredPtr.
+  Επιστρέφει: Την τροποποιημένη συνδεδεμένη λίστα, τον τροποποιημένο πίνακα Node
+                και τον δείκτη FreePtr.
+  Εξοδος:     Μήνυμα γεμάτης λίστας, αν η συνδεδεμένη λίστα είναι γεμάτη
+*/
+{
+  ListPointer TempPtr;
+  GetNode(&TempPtr,FreePtr,Node);
+  if (!FullList(TempPtr)) {
+    if (PredPtr==NilValue)
+    {
+        Node[TempPtr].Data =Item;
+        Node[TempPtr].Next =*List;
+        *List =TempPtr;
+    }
+    else
+      {
+        Node[TempPtr].Data =Item;
+        Node[TempPtr].Next =Node[PredPtr].Next;
+        Node[PredPtr].Next =TempPtr;
+      }
+}
+  else
+    printf("Full List ...\n");
+}
+
+void InsertFreq(ListPointer *List, NodeFreq Node[],ListPointer *FreePtr, ListPointer PredPtr, ListElementType Item1, ListElementType Item2)
+/* Δέχεται:    Μια συνδεδεμένη λίστα, τον πίνακα Node, τον δείκτη PredPtr και
+                ένα στοιχείο Item.
+  Λειτουργία: Εισάγει στη συνδεδεμένη λίστα, αν δεν είναι γεμάτη, το στοιχείο
+                Item μετά από τον κόμβο στον οποίο δείχνει ο δείκτης PredPtr.
+  Επιστρέφει: Την τροποποιημένη συνδεδεμένη λίστα, τον τροποποιημένο πίνακα Node
+                και τον δείκτη FreePtr.
+  Εξοδος:     Μήνυμα γεμάτης λίστας, αν η συνδεδεμένη λίστα είναι γεμάτη
+*/
+{
+  ListPointer TempPtr;
+  GetNodeFreq(&TempPtr,FreePtr,Node);
+  if (!FullList(TempPtr)) {
+    if (PredPtr==NilValue)
+    {
+        Node[TempPtr].klash =Item1;
+        Node[TempPtr].Freq = Item2;
+        Node[TempPtr].Next =*List;
+        *List =TempPtr;
+    }
+    else
+      {
+        Node[TempPtr].klash =Item1;
+        Node[TempPtr].Freq = Item2;
+        Node[TempPtr].Next =Node[PredPtr].Next;
+        Node[PredPtr].Next =TempPtr;
+      }
+}
+  else
+    printf("Full List ...\n");
+}
+
+void Delete(ListPointer *List, NodeType Node[], ListPointer *FreePtr, ListPointer PredPtr)
+/* Δέχεται:   Μια συνδεδεμένη λίστα και τον δείκτη PredPtr που δείχνει
+                στον προηγούμενο κόμβο από αυτόν που θα διαγραφεί.
+  Λειτουργία: Διαγράφει από τη συνδεδεμένη λίστα, αν δεν είναι κενή,
+                τον προηγούμενο κόμβο από αυτόν στον οποίο δείχνει ο PredPtr.
+  Επιστρέφει: Την τροποποιημένη λίστα και το δείκτη FreePtr.
+  Έξοδος:     Μήνυμα κενής λίστας, αν η συνδεδεμένη λίστα είναι κενή
+*/
+{
+  ListPointer TempPtr ;
+
+  if (!EmptyList(*List))
+    if (PredPtr == NilValue)
+    {
+        TempPtr =*List;
+        *List =Node[TempPtr].Next;
+        ReleaseNode(Node,TempPtr,FreePtr);
+    }
+    else
+      {
+        TempPtr =Node[PredPtr].Next;
+        Node[PredPtr].Next =Node[TempPtr].Next;
+        ReleaseNode(Node,TempPtr,FreePtr);
+      }
+  else
+    printf("Empty List ...\n");
+}
+
+void TraverseLinked(ListPointer List, NodeType Node[])
+/* Δέχεται:    Μια συνδεδεμένη λίστα.
+   Λειτουργία: Κάνει διάσχιση της συνδεδεμένης λίστας, αν δεν είναι κενή.
+   Έξοδος:     Εξαρτάται από την επεξεργασία
+*/
+{
+  ListPointer CurrPtr;
+
+  if (!EmptyList(List))
+  {
+      CurrPtr =List;
+      while (CurrPtr != NilValue)
+      {
+          printf("(%d) ",Node[CurrPtr].Data);
+          CurrPtr=Node[CurrPtr].Next;
+       }
+       printf("\n");
+   }
+  else printf("Empty List ...\n");
+}
+
+void TraverseFreqLinked(ListPointer List, NodeFreq Node[])
+/* Δέχεται:    Μια συνδεδεμένη λίστα.
+   Λειτουργία: Κάνει διάσχιση της συνδεδεμένης λίστας, αν δεν είναι κενή.
+   Έξοδος:     Εξαρτάται από την επεξεργασία
+*/
+{
+  ListPointer CurrPtr;
+
+  if (!EmptyList(List))
+  {
+      CurrPtr =List;
+      while (CurrPtr != NilValue)
+      {
+          printf("(%d:%d) ",Node[CurrPtr].klash, Node[CurrPtr].Freq);
+          CurrPtr=Node[CurrPtr].Next;
+       }
+       printf("\n");
+   }
+  else printf("Empty List ...\n");
+}
