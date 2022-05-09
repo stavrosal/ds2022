1d07e18ebbad8502a826b3b4f9121326e848969b
Stavros Almpanopoulos <102695313+stavrosal@users.noreply.github.com>
2022-05-10 00:05:51 +0300

    Update a31f5.c

--- a31f5.c
+++ a31f5.c
@@ -4,9 +4,9 @@
 
 typedef struct
 {
-    char AM[8];
-    char fname[10];
-    char lname[10];
+    char AM[9];
+    char onoma[11];
+    char lname[11];
 }BinTreeElementType;
 
 typedef struct BinTreeNode *BinTreePointer;
@@ -34,19 +34,15 @@ int main()
     boolean found;
 
     BuildBST(&ARoot);
-    InorderTraversal(ARoot);
-    printf("\n");
     for(i=0;i<3;i++)
     {
         printf("Give am:");
         scanf("%s",student.AM);
-        getchar();
         BSTSearch(ARoot,student,&found,&LocPtr,&num);
-        printf("komboi:%d ",num);
         if(found==TRUE)
-            printf("AM:%s Onoma:%s Epwnymo:%s\n",ARoot->Data.AM,ARoot->Data.fname,ARoot->Data.lname);
+            printf("komboi=%d AM=%s Onoma=%s Epwnymo=%s\n",num,LocPtr->Data.AM,LocPtr->Data.onoma,LocPtr->Data.lname);
         else
-            printf(" o foithths den brethike sto DDA\n");
+            printf("komboi=%d o foithths den brethike sto DDA\n",num);
 
     }
 
@@ -54,15 +50,10 @@ int main()
     return 0;
 }
 void InorderTraversal(BinTreePointer Root)
-/* Δέχεται:    Ένα δυαδικό δέντρο με το δείκτη Root να δείχνει στην ρίζα του.
-   Λειτουργία: Εκτελεί ενδοδιατεταγμένη διάσχιση του δυαδικού δέντρου και
-                επεξεργάζεται κάθε κόμβο ακριβώς μια φορά.
-   Εμφανίζει: Το περιεχόμενο του κόμβου, και εξαρτάται από το είδος της επεξεργασίας
-*/
 {
     if (Root!=NULL) {
         InorderTraversal(Root->LChild);
-        printf("%s %s %s",Root->Data.AM,Root->Data.fname,Root->Data.lname);
+        printf("AM:%s Onoma:%s Epwnymo:%s\n",Root->Data.AM,Root->Data.onoma,Root->Data.lname);
         InorderTraversal(Root->RChild);
     }
 }
@@ -70,40 +61,38 @@ void BuildBST(BinTreePointer *Root)
 {
     FILE *infile;
     BinTreeElementType student;
-    int nscan;
-    char temch;
 
-    CreateBST(Root);
+    CreateBST(&(*Root));
     infile=fopen("i31f5.txt ","r");
     if(infile==NULL) printf("Problem Opening File");
     else
         while(!feof(infile))
         {
-            nscan=fscanf(infile,"%[^ ]%[^ ]%[^ ]%c ",student.AM, student.fname, student.lname,&temch);
-            if(nscan!=4 || temch!='\n') exit(1);
+
+            fscanf(infile,"%s%s%s ",student.AM, student.onoma, student.lname);
+            //printf("AM:%s Onoma:%s Epwnymo:%s\n",student.AM,student.onoma,student.lname);
             BSTInsert(Root,student);
         }
     fclose(infile);
 }
 void BSTSearch(BinTreePointer Root, BinTreeElementType KeyValue, boolean *Found, BinTreePointer *LocPtr, int *nodes)
 {
-    (*LocPtr) = Root;
-    (*Found) = FALSE;
-    *nodes=1;
-    while (!(*Found) && (*LocPtr) != NULL)
+    boolean stop;
+
+    *LocPtr = Root;
+    stop = FALSE;
+    (*nodes)=0;
+    while (!stop && *LocPtr != NULL)
     {
+        (*nodes)++;
         if (strcmp(KeyValue.AM,(*LocPtr)->Data.AM)<0)
-        {
-            (*LocPtr) = (*LocPtr)->LChild;
-            *nodes++;
-        }
-        else if (strcmp(KeyValue.AM,(*LocPtr)->Data.AM)>0)
-        {
-            (*LocPtr) = (*LocPtr)->RChild;
-            *nodes+=2;
-        }
-        else (*Found) = TRUE;
+            *LocPtr = (*LocPtr)->LChild;
+        else
+            if (strcmp(KeyValue.AM, (*LocPtr)->Data.AM)>0)
+                *LocPtr = (*LocPtr)->RChild;
+            else stop = TRUE;
     }
+   *Found=stop;
 }
 
 void CreateBST(BinTreePointer *Root)
@@ -125,7 +114,7 @@ void BSTInsert(BinTreePointer *Root, BinTreeElementType Item)
     Found = FALSE;
     while (!Found && LocPtr != NULL) {
         Parent = LocPtr;
-        if (strcmp(Item.AM,LocPtr->Data.AM)<0)
+        if (strcmp(Item.AM, LocPtr->Data.AM)<0)
             LocPtr = LocPtr ->LChild;
         else if (strcmp(Item.AM,LocPtr ->Data.AM)>0)
             LocPtr = LocPtr ->RChild;
@@ -141,10 +130,9 @@ void BSTInsert(BinTreePointer *Root, BinTreeElementType Item)
         LocPtr ->RChild = NULL;
         if (Parent == NULL)
             *Root = LocPtr;
-        else if (strcpy(Item.AM, Parent ->Data.AM)<0)
+        else if (strcmp(Item.AM,Parent ->Data.AM)<0)
             Parent ->LChild = LocPtr;
         else
             Parent ->RChild = LocPtr;
     }
 }
-
