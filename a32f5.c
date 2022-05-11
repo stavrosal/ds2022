4f5621201c8759b299ff3324192a74f72511d374
LazAlexandridis <102719725+LazAlexandridis@users.noreply.github.com>
2022-05-11 21:56:05 +0300

    Add files via upload

---
+++ a32f5.c
@@ -0,0 +1,216 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char BinTreeElementType;
+
+typedef struct BinTreeNode *BinTreePointer;
+typedef struct BinTreeNode {
+    BinTreeElementType Data;
+    BinTreePointer LChild, RChild;
+} BinTreeNode;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+
+void CreateBST(BinTreePointer *Root);
+boolean BSTEmpty(BinTreePointer Root);
+void RecBSTInsert(BinTreePointer *Root, BinTreeElementType Item);
+void RecBSTSearch(BinTreePointer Root, BinTreeElementType KeyValue, boolean *Found, BinTreePointer *LocPtr);
+void RecBSTDelete(BinTreePointer *Root, BinTreeElementType KeyValue);
+void RecBSTInorder(BinTreePointer Root);
+void RecBSTPreorder(BinTreePointer Root);
+void RecBSTPostorder(BinTreePointer Root);
+int LeftNodeCount(BinTreePointer Root);
+
+int main()
+{
+
+    char synolo[]="ALGORITHM";
+    int i, left_nodes;
+    BinTreePointer ARoot,BRoot;
+
+    CreateBST(&ARoot);
+    CreateBST(&BRoot);
+
+
+    for(i=0;i<9;i++)
+    {
+        RecBSTInsert(&ARoot,synolo[i]);
+        RecBSTInsert(&BRoot,synolo[8-i]);
+    }
+    printf("First tree with inorder traverse\n");
+    RecBSTInorder(ARoot);
+    printf("\nSecond tree with inorder traverse\n");
+    RecBSTInorder(BRoot);
+    left_nodes=LeftNodeCount(ARoot);
+    printf("\nFirst tree LeftNodeCount =%d\n",left_nodes);
+    left_nodes=LeftNodeCount(BRoot);
+    printf("Second tree LeftNodeCount =%d\n",left_nodes);
+
+    return 0;
+}
+int LeftNodeCount(BinTreePointer Root)
+{
+    int counter=0;
+
+    if(Root->LChild!=NULL)
+    {
+        counter++;
+        counter+=LeftNodeCount(Root->LChild);
+    }
+    if(Root->RChild!=NULL)
+        counter+=LeftNodeCount(Root->RChild);
+
+    return counter;
+}
+void CreateBST(BinTreePointer *Root)
+{
+    *Root = NULL;
+}
+
+boolean BSTEmpty(BinTreePointer Root)
+{
+    return (Root==NULL);
+}
+
+void RecBSTInsert(BinTreePointer *Root, BinTreeElementType Item)
+{
+    if (BSTEmpty(*Root)) {
+        (*Root) = (BinTreePointer)malloc(sizeof (struct BinTreeNode));
+        (*Root) ->Data = Item;
+        (*Root) ->LChild = NULL;
+        (*Root) ->RChild = NULL;
+    }
+    else
+        if (Item < (*Root) ->Data)
+            RecBSTInsert(&(*Root) ->LChild,Item);
+        else if (Item > (*Root) ->Data)
+            RecBSTInsert(&(*Root) ->RChild,Item);
+        else
+            printf("TO STOIXEIO EINAI HDH STO DDA\n");
+}
+
+void RecBSTSearch(BinTreePointer Root, BinTreeElementType KeyValue,
+                    boolean *Found, BinTreePointer *LocPtr)
+/* Δέχεται:    Ένα ΔΔΑ με το δείκτη Root να δείχνει στη ρίζα του και μια τιμή KeyValue.
+   Λειτουργία: Αναζητά στο ΔΔΑ έναν κόμβο με τιμή KeyValue στο πεδίο κλειδί του.
+   Επιστρέφει: Η Found έχει τιμή TRUE και ο δείκτης LocPtr δείχνει στον κόμβο που
+                περιέχει την τιμή KeyValue, αν η αναζήτηση είναι επιτυχής.
+                Διαφορετικά η Found έχει τιμή FALSE
+*/
+{
+    if (BSTEmpty(Root))
+        *Found=FALSE;
+    else
+        if (KeyValue < Root->Data)
+            RecBSTSearch(Root->LChild, KeyValue, &(*Found), &(*LocPtr));
+        else
+            if (KeyValue > Root->Data)
+                RecBSTSearch(Root->RChild, KeyValue, &(*Found), &(*LocPtr));
+            else
+                {
+                    *Found = TRUE;
+                    *LocPtr=Root;
+                }
+}
+
+void RecBSTDelete(BinTreePointer *Root, BinTreeElementType KeyValue)
+/* Δέχεται:  Ένα ΔΔΑ με το δείκτη Root να δείχνει στη ρίζα του και μια τιμή KeyValue.
+   Λειτουργία: Προσπαθεί να βρει έναν κόμβο στο ΔΔΑ που να περιέχει την τιμή
+                KeyValue στο πεδίο κλειδί του τμήματος δεδομένων του και,
+                αν τον βρει, τον διαγράφει από το ΔΔΑ.
+  Επιστρέφει: Το τροποποιημένο ΔΔΑ με τον δείκτη Root να δείχνει στη ρίζα του.
+*/
+{
+
+   BinTreePointer TempPtr;          //* true AN TO STOIXEIO KeyValue EINAI STOIXEIO TOY DDA *)
+
+    if (BSTEmpty(*Root))          //* ΑΔΕΙΟ ΔΕΝΔΡΟ ΤΟ KeyValue ΔΕ ΘΑ ΒΡΕΘΕΙ *)
+        printf("TO STOIXEIO DEN BRE8HKE STO DDA\n");
+     else
+         //* αναζήτησε αναδρομικά τον κόμβο που περιέχει την τιμή KeyValue και διάγραψέ τον
+          if (KeyValue < (*Root)->Data)
+             RecBSTDelete(&((*Root)->LChild), KeyValue);       //* ΑΡΙΣΤΕΡΟ ΥΠΟΔΕΝΔΡΟ *
+          else
+            if (KeyValue > (*Root)->Data)
+                  RecBSTDelete(&((*Root)->RChild), KeyValue);   //* ΔΕΞΙ  ΥΠΟΔΕΝΔΡΟ *
+            else                                       //* TO KeyValue ΒΡΕΘΗΚΕ ΔΙΑΓΡΑΦΗ ΤΟΥ ΚΟΜΒΟΥ *)
+                if ((*Root)->LChild ==NULL)
+                 {
+                      TempPtr = *Root;
+                      *Root = (*Root)->RChild;      //* ΔΕΝ ΕΧΕΙ ΑΡΙΣΤΕΡΟ ΠΑΙΔΙ *)
+                      free(TempPtr);
+                 }
+                else if ((*Root)->RChild == NULL)
+                   {
+                        TempPtr = *Root;
+                        *Root = (*Root)->LChild;      //* ΕΧΕΙ ΑΡΙΣΤΕΡΟ ΠΑΙΔΙ, ΑΛΛΑ ΟΧΙ ΔΕΞΙ *)
+                        free(TempPtr);
+                   }
+                   else                               //* ΕΧΕΙ 2 ΠΑΙΔΙΑ *)
+                   {
+                        //* ΕΥΡΕΣΗ ΤΟΥ INORDER ΑΠΟΓΟΝΟΥ ΤΟΥ *)
+                        TempPtr = (*Root)->RChild;
+                        while (TempPtr->LChild != NULL)
+                              TempPtr = TempPtr->LChild;
+                        /* ΜΕΤΑΚΙΝΗΣΗ ΤΟΥ ΑΠΟΓΟΝΟΥ ΤΗς ΡΙΖΑΣ ΤΟΥ ΥΠΟΔΕΝΔΡΟΥ
+                        ΠΟΥ ΕΞΕΤΑΖΕΤΑΙ ΚΑΙ ΔΙΑΓΡΑΦΗ ΤΟΥ ΑΠΟΓΟΝΟΥ ΚΟΜΒΟΥ */
+                        (*Root)->Data = TempPtr->Data;
+                        RecBSTDelete(&((*Root)->RChild), (*Root)->Data);
+                   }
+}
+
+void RecBSTInorder(BinTreePointer Root)
+/* Δέχεται:    Ένα δυαδικό δέντρο με το δείκτη Root να δείχνει στην ρίζα του.
+   Λειτουργία: Εκτελεί ενδοδιατεταγμένη διάσχιση του δυαδικού δέντρου και
+                επεξεργάζεται κάθε κόμβο ακριβώς μια φορά.
+   Εμφανίζει: Το περιεχόμενο του κόμβου, και εξαρτάται από το είδος της επεξεργασίας
+*/
+{
+    if (Root!=NULL) {
+    //   printf("L");
+        RecBSTInorder(Root->LChild);
+        printf("%c ",Root->Data);
+    //    printf("R");
+        RecBSTInorder(Root->RChild);
+    }
+   // printf("U");
+}
+
+void RecBSTPreorder(BinTreePointer Root)
+/* Δέχεται:    Ένα δυαδικό δέντρο με το δείκτη Root να δείχνει στην ρίζα του.
+   Λειτουργία: Εκτελεί προδιατεταγμένη διάσχιση του δυαδικού δέντρου και
+                επεξεργάζεται κάθε κόμβο ακριβώς μια φορά.
+   Εμφανίζει: Το περιεχόμενο του κόμβου, και εξαρτάται από το είδος της επεξεργασίας
+*/
+{
+    if (Root!=NULL) {
+        printf("%d ",Root->Data);
+       // printf("L");
+        RecBSTPreorder(Root->LChild);
+      //  printf("R");
+        RecBSTPreorder(Root->RChild);
+    }
+   // printf("U");
+}
+
+void RecBSTPostorder(BinTreePointer Root)
+/* Δέχεται:    Ένα δυαδικό δέντρο με το δείκτη Root να δείχνει στην ρίζα του.
+   Λειτουργία: Εκτελεί μεταδιατεταγμένη διάσχιση του δυαδικού δέντρου και
+                επεξεργάζεται κάθε κόμβο ακριβώς μια φορά.
+   Εμφανίζει: Το περιεχόμενο του κόμβου, και εξαρτάται από το είδος της επεξεργασίας
+*/
+{
+    if (Root!=NULL) {
+      //  printf("L");
+        RecBSTPostorder(Root->LChild);
+     //   printf("R");
+        RecBSTPostorder(Root->RChild);
+        printf("%d ",Root->Data);
+    }
+   // printf("U");
+}
+
