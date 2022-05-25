fa8e9e2e282b74149c5b92aaa41552d24d703ba9
Stavros Almpanopoulos <102695313+stavrosal@users.noreply.github.com>
2022-05-25 12:08:00 +0300

    Add files via upload

---
+++ a10f6.c
@@ -0,0 +1,323 @@
+//filename : a10f6.c
+
+#include <stdio.h>
+#include <string.h>
+
+#define HMax 31              /*     το μέγεθος του πίνακα HashTable*/
+
+#define VMax 30             /*το μέγεθος της λίστας*/
+
+#define EndOfList -1        /* σημαία που σηματοδοτεί το τέλος της λίστας
+                                και της κάθε υπολίστας συνωνύμων */
+
+typedef struct {
+    char name[6];
+    int value;
+} ListElementType;  /*τύπος δεδομένων για τα στοιχεία της λίστας*/
+
+typedef int KeyType;
+
+typedef struct {
+	KeyType key;
+	ListElementType Data;
+	int Link;
+} ListElm;
+
+typedef struct  {
+	int HashTable[HMax];   // πίνακας δεικτών προς τις υπολίστες συνωνύμων
+    int Size;                // πλήθος εγγραφών της λίστας List
+	int SubListPtr;          // Dδείκτης σε μια υπολίστα συνωνύμων
+    int StackPtr;           // δείκτης προς την πρώτη ελεύθερη θέση της λίστας List
+	ListElm List[VMax];
+} HashListType;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+void CreateHashList(HashListType *HList);
+int HashKey(KeyType key);
+boolean FullHashList(HashListType HList);
+void SearchSynonymList(HashListType HList,KeyType KeyArg,int *Loc,int *Pred);
+void SearchHashList(HashListType HList,KeyType KeyArg,int *Loc,int *Pred);
+void AddRec(HashListType *HList,ListElm InRec);
+void DeleteRec(HashListType *HList,KeyType DelKey);
+void Print_HashList(HashListType HList);
+
+int FoldKey(char input[]);
+
+int main(){
+    char name[6], t1[6], t2[6];
+    int i, value, key;
+    int Loc, Pred, Loc2, Pred2;
+    HashListType HList;
+
+    CreateHashList(&HList);
+
+    ListElm item;
+
+    printf("Declare variables\n");
+    for (i=0; i<4; i++){
+        printf("Enter the variable name: ");
+        scanf("%5s", name);
+
+        printf("Enter variable value: ");
+        scanf("%d", &value);
+
+
+        item.key = FoldKey(name);
+        item.Link = -1;
+        strcpy(item.Data.name, name);
+        item.Data.value = value;
+
+        AddRec(&HList, item);
+    }
+
+    Print_HashList(HList);
+
+    printf("Multiply Variables\n");
+    printf("Enter the first variable: ");
+    scanf("%5s", t1);
+    printf("Enter the second variable: ");
+    scanf("%5s", t2);
+
+    SearchHashList(HList, FoldKey(t1), &Loc, &Pred);
+    if (Loc == -1)
+        printf("Undeclared variable %s", t1);
+
+    SearchHashList(HList, FoldKey(t2), &Loc2, &Pred2);
+    if (Loc2 == -1)
+        printf("Undeclared variable %s", t2);
+
+    if (Loc != -1 && Loc2 != -1){
+        printf("%sx%s=%d", t1, t2, HList.List[Loc].Data.value*HList.List[Loc2].Data.value);
+    }
+
+
+    printf("\nAdd variables\n");
+    printf("Enter the first variable: ");
+    scanf("%5s", t1);
+    printf("Enter the second variable: ");
+    scanf("%5s", t2);
+
+    SearchHashList(HList, FoldKey(t1), &Loc, &Pred);
+    if (Loc == -1)
+        printf("Undeclared variable %s", t1);
+
+    SearchHashList(HList, FoldKey(t2), &Loc2, &Pred2);
+    if (Loc2 == -1)
+        printf("Undeclared variable %s", t2);
+
+    if (Loc != -1 && Loc2 != -1){
+        printf("%s+%s=%d", t1, t2, HList.List[Loc].Data.value+HList.List[Loc2].Data.value);
+    }
+
+    return 0;
+}
+
+int FoldKey(char input[]){
+    int i, value=0;
+
+    for (i=0; i<strlen(input); i++){
+        value = value + (i+1)*(input[i]-64);
+    }
+
+    return value;
+}
+
+int HashKey(KeyType key)
+/* Δέχεται:    Την τιμή key ενός κλειδιού.
+   Λειτουργία: Βρίσκει την τιμή κατακερματισμού HValue για το κλειδί Key.
+   Επιστρέφει: Την τιμή κατακερματισμού HValue
+*/
+{
+	/*σε περίπτωση που το KeyType δεν είναι ακέραιος
+	θα πρέπει να μετατρέπεται κατάλληλα το κλειδί σε αριθμό*/
+	return key%HMax;
+}
+
+void CreateHashList(HashListType *HList)
+/* Λειτουργία: Δημιουργεί μια δομή HList.
+   Επιστρέφει: Την δομή HList
+*/
+{
+	int index;
+
+	HList->Size=0;           //ΔΗΜΙΟΥΡΓΕΙ ΜΙΑ ΚΕΝΗ ΛΙΣΤΑ
+	HList->StackPtr=0;       //ΔΕΙΚΤΗς ΣΤΗ ΚΟΡΥΦΗ ΤΗΣ ΣΤΟΙΒΑΣ ΤΩΝ ΕΛΕΥΘΕΡΩΝ ΘΕΣΕΩΝ
+
+    /*ΑΡΧΙΚΟΠΟΙΕΙ ΤΟΝ ΠΙΝΑΚΑ HashTable ΤΗΣ ΔΟΜΗΣ HList ΩΣΤΕ ΚΑΘΕ ΣΤΟΙΧΕΙΟΥ ΤΟΥ
+        ΝΑ ΕΧΕΙ ΤΗ ΤΙΜΗ EndOfList (-1)*/
+    index=0;
+	while (index<HMax)
+	{
+		HList->HashTable[index]=EndOfList;
+		index=index+1;
+    }
+
+     //Δημιουργία της στοίβας των ελεύθερων θέσεων στη λίστα HList
+    index=0;
+	while(index < VMax-1)
+	{
+		HList->List[index].Link=index+1;
+		index=index+1;
+	}
+	HList->List[index].Link=EndOfList;
+}
+
+boolean FullHashList(HashListType HList)
+/* Δέχεται:    Μια δομή HList.
+   Λειτουργία: Ελέγχει αν η λίστα List της δομής HList είναι γεμάτη.
+   Επιστρέφει: TRUE αν η λίστα List είναι γεμάτη, FALSE διαφορετικά.
+*/
+{
+	return(HList.Size==VMax);
+}
+
+void SearchSynonymList(HashListType HList,KeyType KeyArg,int *Loc,int *Pred)
+/* Δέχεται:     Μια δομή HList και μια τιμή κλειδιού KeyArg.
+    Λειτουργία: Αναζητά μια εγγραφή με κλειδί KeyArg στην υπολίστα συνωνύμων.
+    Επιστρέφει: Τη θέση Loc της εγγραφής και τη θέση Pred της προηγούμενης
+                εγγραφής στην υπολίστα
+*/
+{
+	int Next;
+	Next=HList.SubListPtr;
+	*Loc=-1;
+	*Pred=-1;
+	while(Next!=EndOfList)
+	{
+		if (HList.List[Next].key==KeyArg)
+		{
+			*Loc=Next;
+			Next=EndOfList;
+		}
+		else
+		{
+			*Pred=Next;
+			Next=HList.List[Next].Link;
+		}
+	}
+}
+void SearchHashList(HashListType HList,KeyType KeyArg,int *Loc,int *Pred)
+/* Δέχεται:     Μια δομή HList και μια τιμή κλειδιού KeyArg.
+    Λειτουργία: Αναζητά μια εγγραφή με κλειδί KeyArg στη δομή HList.
+    Επιστρέφει: Τη θέση Loc της εγγραφής και τη θέση Pred της
+                προηγούμενης εγγραφής της υπολίστας στην οποία ανήκει.
+                Αν δεν υπάρχει εγγραφή με κλειδί KeyArg τότε Loc=Pred=-1
+*/
+{
+	int HVal;
+	HVal=HashKey(KeyArg);
+    if (HList.HashTable[HVal]==EndOfList)
+	{
+		*Pred=-1;
+		*Loc=-1;
+	}
+	else
+	{
+		HList.SubListPtr=HList.HashTable[HVal];
+		SearchSynonymList(HList,KeyArg,Loc,Pred);
+	}
+}
+
+void AddRec(HashListType *HList,ListElm InRec)
+/* Δέχεται:    Μια δομή HList και μια εγγραφή InRec.
+   Λειτουργία: Εισάγει την εγγραφή InRec στη λίστα List, αν δεν είναι γεμάτη,
+                και ενημερώνει τη δομή HList.
+   Επιστρέφει: Την τροποποιημένη δομή HList.
+   Έξοδος:     Μήνυμα γεμάτης λίστας, αν η List είναι γεμάτη, διαφορετικά,
+                αν υπάρχει ήδη εγγραφή με το ίδιο κλειδί,
+                εμφάνιση αντίστοιχου μηνύματος
+*/
+{
+	int Loc, Pred, New, HVal;
+
+   // New=0;
+	if(!(FullHashList(*HList)))
+	{
+		Loc=-1;
+		Pred=-1;
+		SearchHashList(*HList,InRec.key,&Loc,&Pred);
+		if(Loc==-1)
+		{
+			HList->Size=HList->Size +1;
+			New=HList->StackPtr;
+			HList->StackPtr=HList->List[New].Link;
+			HList->List[New]=InRec;
+			if (Pred==-1)
+			{
+			    HVal=HashKey(InRec.key);
+                HList->HashTable[HVal]=New;
+				HList->List[New].Link=EndOfList;
+			}
+			else
+			{
+                HList->List[New].Link=HList->List[Pred].Link;
+				HList->List[Pred].Link=New;
+			}
+		}
+
+		else
+		{
+			printf("YPARXEI HDH EGGRAFH ME TO IDIO KLEIDI \n");
+		}
+	}
+	else
+	{
+		printf("Full list...");
+	}
+}
+void DeleteRec(HashListType *HList,KeyType DelKey)
+/* DEXETAI:    TH DOMH (HList) KAI To KLEIDI (DelKey) THS EGGRAFHS
+               POY PROKEITAI NA DIAGRAFEI
+   LEITOYRGIA: DIAGRAFEI, THN EGGRAFH ME KLEIDI (DelKey) APO TH
+               LISTA (List), AN YPARXEI ENHMERWNEI THN DOMH HList
+   EPISTREFEI: THN TROPOPOIHMENH DOMH (HList)
+   EJODOS:     "DEN YPARXEI EGGRAFH ME KLEIDI" MHNYMA
+*/
+{
+	int Loc, Pred, HVal;
+
+	SearchHashList(*HList,DelKey,&Loc,&Pred);
+	if(Loc!=-1)
+	{
+		if(Pred!=-1)
+		{
+			HList->List[Pred].Link=HList->List[Loc].Link;
+		}
+		else
+		{
+			HVal=HashKey(DelKey);
+		    HList->HashTable[HVal]=HList->List[Loc].Link;
+		}
+		HList->List[Loc].Link=HList->StackPtr;
+		HList->StackPtr=Loc;
+		HList->Size=HList->Size -1;
+	}
+	else
+	{
+		printf("DEN YPARXEI  EGGRAFH ME KLEIDI %d \n",DelKey);
+	}
+}
+
+void Print_HashList(HashListType HList)
+{
+   int i, SubIndex;
+
+          for (i=0; i<HMax;i++)
+          {
+              SubIndex = HList.HashTable[i];
+              while ( SubIndex != EndOfList )
+              {
+                   printf("[%d, %s %d, %d]", HList.List[SubIndex].key, HList.List[SubIndex].Data.name, HList.List[SubIndex].Data.value, HList.List[SubIndex].Link);
+                   printf(" -> ");
+                   SubIndex = HList.List[SubIndex].Link;
+                   if (SubIndex == EndOfList)
+                        printf("TELOS % dHS YPO-LISTAS\n", i);
+               }
+
+          }
+
+}
+
